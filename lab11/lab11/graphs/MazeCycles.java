92b327964d0ffeb3edab22e73e5d3f203b255f3e
Filtee <christopherfabler@gmail.com>
2023-09-16 16:33:35 +0800

    lab 11: Finished!

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -12,13 +14,52 @@ public class MazeCycles extends MazeExplorer {
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        s = 0;
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(s);
     }
 
     // Helper methods go here
+    private int s;
+    private int cycleNode;
+    private boolean cycleFound = false;
+    private boolean cycleComplete = false;
+    private Maze maze;
+
+    private void dfs(int v) {
+        marked[v] = true;
+
+        for (int w : maze.adj(v)) {
+            if (marked[w] && distTo[w] != distTo[v] - 1) {
+                cycleFound = true;
+                cycleNode = w;
+                edgeTo[w] = v;
+                announce();
+            }
+
+            if (cycleFound) {
+                if (v == cycleNode) {
+                    cycleComplete = true;
+                }
+                return;
+            }
+
+            if (!marked[w]) {
+                distTo[w] = distTo[v] + 1;
+                dfs(w);
+
+                if (!cycleComplete) {
+                    edgeTo[w] = v;
+                    announce();
+                }
+            }
+        }
+    }
 }
 
