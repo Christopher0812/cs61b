44ec2b5ec0236f4e2e87cf8f6ded8310d4e8ec97
Filtee <christopherfabler@gmail.com>
2023-10-02 13:45:51 +0800

    Lab13: Ultimate!

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
  * Class for doing Radix sort
  *
  * @author Akhil Batra, Alexander Hwang
- *
  */
 public class RadixSort {
     /**
@@ -14,7 +13,6 @@ public class RadixSort {
      * The Strings can be variable length (all Strings are not constrained to 1 length)
      *
      * @param asciis String[] that needs to be sorted
-     *
      * @return String[] the sorted array
      */
     public static String[] sort(String[] asciis) {
@@ -26,7 +24,7 @@ public class RadixSort {
         }
 
         for (int i = 0; i < max; i++) {
-            sortHelperLSD(asciisCopy, i);
+            sortHelperLSD(asciisCopy, max - i - 1);
         }
 
         return asciisCopy;
@@ -42,8 +40,9 @@ public class RadixSort {
     /**
      * LSD helper method that performs a destructive counting sort the array of
      * Strings based off characters at a specific index.
+     *
      * @param asciis Input array of Strings
-     * @param index The position to sort the Strings on.
+     * @param index  The position to sort the Strings on.
      */
     private static void sortHelperLSD(String[] asciis, int index) {
         // gather all the counts for each value
@@ -75,10 +74,9 @@ public class RadixSort {
      * Destructive method that changes the passed in array, asciis.
      *
      * @param asciis String[] to be sorted
-     * @param start int for where to start sorting in this method (includes String at start)
-     * @param end int for where to end sorting in this method (does not include String at end)
-     * @param index the index of the character the method is currently sorting on
-     *
+     * @param start  int for where to start sorting in this method (includes String at start)
+     * @param end    int for where to end sorting in this method (does not include String at end)
+     * @param index  the index of the character the method is currently sorting on
      **/
     private static void sortHelperMSD(String[] asciis, int start, int end, int index) {
         // Optional MSD helper method for optional MSD radix sort
@@ -86,9 +84,9 @@ public class RadixSort {
     }
 
     public static void main(String[] args) {
-        String[] students = new String[]{"Vanessa", "Alice", "Ab", "Ethan"};
+        String[] students = new String[]{"Vanessa", "Alice", "Am", "Ethan"};
 
-        String[] expected = new String[]{"Ab",  "Alice", "Ethan", "Vanessa"};
+        String[] expected = new String[]{"Alice", "Am", "Ethan", "Vanessa"};
 
         System.out.println(Arrays.toString(expected));
         System.out.println(Arrays.toString(RadixSort.sort(students)));
